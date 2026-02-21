46d97a09c5b55b2ec62332c9696cfe9292d50598
IbnuNabel <149796661+IbnuNabel@users.noreply.github.com>
2026-02-21 16:11:11 +0700

    Add User model with find and create methods

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,21 @@
+const db = require('../config/db');
 
+class User {
+    // Fungsi untuk mencari user berdasarkan username
+    static findByUsername(username) {
+        return db.users.find(user => user.username === username);
+    }
+
+    // Fungsi tambahan jika nanti ingin fitur registrasi
+    static create(username, password) {
+        const newUser = {
+            id: db.users.length + 1,
+            username,
+            password 
+        };
+        db.users.push(newUser);
+        return newUser;
+    }
+}
+
+module.exports = User;
