ccab7333a885707c5570339de548a58f4df6b93f
IbnuNabel <149796661+IbnuNabel@users.noreply.github.com>
2026-02-22 14:30:27 +0700

    Add status field to user objects in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
 const db = {
   users: [
-    { id: 1, username: "shinta", password: "036" },
-    { id: 2, username: "catherine", password: "042" },
-    { id: 3, username: "ibnu", password: "040" },
-    { id: 4, username: "safira", password: "007" },
+    { id: 1, username: "shinta", password: "036", status: "online" },
+    { id: 2, username: "catherine", password: "042", status: "online" },
+    { id: 3, username: "ibnu", password: "040", status: "online" },
+    { id: 4, username: "safira", password: "007", status: "online" },
   ],
   messages: [],
 };
