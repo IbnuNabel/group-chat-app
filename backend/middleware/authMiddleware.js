ec6a4c554277b62cac9be86cdcae706cfe7fae16
catherinenathania <149342934+catherinenathania@users.noreply.github.com>
2026-02-22 18:10:18 +0700

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,7 +4,6 @@ const { jwtSecret } = require("../config/authConfig");
 module.exports = function (req, res, next) {
   const authHeader = req.headers["authorization"];
 
-  // 1. Cek apakah header ada DAN dimulai dengan kata "Bearer "
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({
       success: false,
@@ -12,21 +11,17 @@ module.exports = function (req, res, next) {
     });
   }
 
-  // 2. Ambil token setelah kata "Bearer "
   const token = authHeader.split(" ")[1];
 
   try {
-    // 3. Verifikasi token
+
     const decoded = jwt.verify(token, jwtSecret);
 
-    // 4. Simpan hasil decode ke req.user
-    // Karena di authController kamu isi { username: user.username }, 
-    // maka req.user sekarang berisi objek tersebut.
     req.user = decoded;
 
     next();
   } catch (err) {
-    // Jika token expired atau dimodifikasi, lempar error 403
+    
     return res.status(403).json({
       success: false,
       message: "Sesi telah berakhir atau token tidak valid",
