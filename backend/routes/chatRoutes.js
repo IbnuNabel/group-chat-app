80298b91949e18a7e902f0ddd7a6df5c62060d7a
Shinta2374 <149783744+Shinta2374@users.noreply.github.com>
2026-02-22 12:16:13 +0700

    Update chatRoutes.js

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const chatController = require('../controllers/chatController');
+const authMiddleware = require('../middleware/authMiddleware');
+
 
 // Import middleware dari Anggota 3 (asumsi file sudah ada)
 // const authMiddleware = require('../middleware/authMiddleware');
