bc465335c4bab40c4e0d062b2fa31b273300fba7
IbnuNabel <149796661+IbnuNabel@users.noreply.github.com>
2026-02-22 15:56:35 +0700

    Update username from 'safira' to 'shafira'

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const db = {
     { id: 1, username: "shinta", password: "036", status: "offline" },
     { id: 2, username: "catherine", password: "042", status: "offline" },
     { id: 3, username: "ibnu", password: "040", status: "offline" },
-    { id: 4, username: "safira", password: "007", status: "offline" },
+    { id: 4, username: "shafira", password: "007", status: "offline" },
   ],
   messages: [],
 };
