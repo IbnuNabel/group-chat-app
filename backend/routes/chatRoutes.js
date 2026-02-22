c4cf10db9cc4a511c8cb1d19bf2713722bed5acb
catherinenathania <149342934+catherinenathania@users.noreply.github.com>
2026-02-22 18:11:25 +0700

    Update chatRoutes.js

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -3,15 +3,7 @@ const router = express.Router();
 const chatController = require('../controllers/chatController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-
-// Import middleware dari Anggota 3 (asumsi file sudah ada)
-// const authMiddleware = require('../middleware/authMiddleware');
-
-// Jalur: POST /api/chat/send-message
-// Kita tambahkan 'authMiddleware' di tengah untuk memverifikasi token sebelum masuk ke controller
 router.post('/send-message', authMiddleware, chatController.sendMessage);
-
-// Jalur: GET /api/chat/get-messages
 router.get('/get-messages', authMiddleware, chatController.getMessages);
 
 module.exports = router;
