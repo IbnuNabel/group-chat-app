ce755d7d2818273e3941202e793ae95d3f23be0d
Shinta2374 <149783744+Shinta2374@users.noreply.github.com>
2026-02-22 12:18:59 +0700

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,40 +1,42 @@
-const db = require('../config/db');
-// Nanti Anggota 3 akan import library JWT di sini:
-// const jwt = require('jsonwebtoken');
+const db = require("../config/db");
+const jwt = require("jsonwebtoken");
+const { jwtSecret, jwtExpires } = require("../config/authConfig");
 
 const login = (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    // 1. Validasi Kredensial (Tugas Backend)
-    // Mencari user di "database" dummy kita
-    const user = db.users.find(u => u.username === username && u.password === password);
+  // 1. Validasi Kredensial (Tugas Backend)
+  // Mencari user di "database" dummy kita
+  const user = db.users.find(
+    (u) => u.username === username && u.password === password,
+  );
 
-    if (!user) {
-        return res.status(401).json({ 
-            success: false, 
-            message: 'Username atau password salah!' 
-        });
-    }
-
-    // 2. Generate JWT (Area Anggota 3)
-    // Sementara kita pakai string dummy agar login Anggota 2 (Frontend) bisa jalan dulu
-    const token = "DUMMY_TOKEN_AKAN_DIGANTI_OLEH_ANGGOTA_3";
-
-    res.status(200).json({
-        success: true,
-        message: 'Login berhasil',
-        token: token, // Token ini yang akan disimpan Anggota 4 di Web Storage
-        user: { username: user.username }
+  if (!user) {
+    return res.status(401).json({
+      success: false,
+      message: "Username atau password salah!",
     });
+  }
+
+  const token = jwt.sign({ username: user.username }, jwtSecret, {
+    expiresIn: jwtExpires,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Login berhasil",
+    token: token, // Token ini yang akan disimpan Anggota 4 di Web Storage
+    user: { username: user.username },
+  });
 };
 
 const logout = (req, res) => {
-    // Di sisi server, kita cukup beri respon sukses.
-    // Penghapusan token yang sebenarnya dilakukan oleh Anggota 4 di frontend.
-    res.status(200).json({ 
-        success: true, 
-        message: 'Berhasil keluar dari sesi' 
-    });
+  // Di sisi server, kita cukup beri respon sukses.
+  // Penghapusan token yang sebenarnya dilakukan oleh Anggota 4 di frontend.
+  res.status(200).json({
+    success: true,
+    message: "Berhasil keluar dari sesi",
+  });
 };
 
 module.exports = { login, logout };
