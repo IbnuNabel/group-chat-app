d5fcb56ce69d17b4a1a71ba813fb1c48993eb1ad
IbnuNabel <149796661+IbnuNabel@users.noreply.github.com>
2026-02-22 15:09:37 +0700

    Add auth middleware to auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // Jalur: POST /api/auth/login
 router.post('/login', authController.login);
