5a877ec16156236093cb683612e77d46e4cb2199
catherinenathania <149342934+catherinenathania@users.noreply.github.com>
2026-02-22 18:10:47 +0700

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,10 @@
 const db = require('../config/db');
 
 class User {
-    // Fungsi untuk mencari user berdasarkan username
     static findByUsername(username) {
         return db.users.find(user => user.username === username);
     }
 
-    // Fungsi tambahan jika nanti ingin fitur registrasi
     static create(username, password) {
         const newUser = {
             id: db.users.length + 1,
