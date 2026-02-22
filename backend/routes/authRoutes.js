1b5b300c3d56b059d32a6dc12218909bdfbad5c8
catherinenathania <149342934+catherinenathania@users.noreply.github.com>
2026-02-22 18:11:07 +0700

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,13 +3,10 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-// Jalur: POST /api/auth/login
 router.post('/login', authController.login);
 
-// Jalur: POST /api/auth/logout
 router.post('/logout', authMiddleware, authController.logout);
 
-// Tambahkan baris ini di bawah rute login/logout
 router.get('/users', authMiddleware, authController.getUsers);
 
 module.exports = router;
