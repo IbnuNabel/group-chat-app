c5da41bc4fdcbc77c2e74fb182e7044ab3313774
IbnuNabel <149796661+IbnuNabel@users.noreply.github.com>
2026-02-22 15:01:41 +0700

    Remove unused authMiddleware import
    Removed unused authMiddleware import from authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const authMiddleware = require('../middleware/authMiddleware');
 
 // Jalur: POST /api/auth/login
 router.post('/login', authController.login);
