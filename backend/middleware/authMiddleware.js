d56c1996a6b3ba4ec178650ed301a90ae9b406e2
Shinta2374 <149783744+Shinta2374@users.noreply.github.com>
2026-02-22 13:12:05 +0700

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,1 +1,45 @@
+// middleware/authMiddleware.js
+
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../config/authConfig");
+
+module.exports = function (req, res, next) {
+  // Ambil header authorization
+  const authHeader = req.headers["authorization"];
+
+  // Jika header tidak ada
+  if (!authHeader) {
+    return res.status(401).json({
+      success: false,
+      message: "Token tidak ditemukan",
+    });
+  }
+
+  // Format harus: Bearer <token>
+  const token = authHeader.split(" ")[1];
+
+  // Jika token kosong / format salah
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Format token salah",
+    });
+  }
+
+  try {
+    // Verifikasi token
+    const decoded = jwt.verify(token, jwtSecret);
+
+    // Simpan data user hasil decode ke request
+    req.user = decoded;
+
+    // lanjut ke controller berikutnya
+    next();
+  } catch (err) {
+    return res.status(403).json({
+      success: false,
+      message: "Token tidak valid",
+    });
+  }
+};
 
