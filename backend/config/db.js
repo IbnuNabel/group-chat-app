c3f6464017dfa26c81d91cec78badd137176832c
Shinta2374 <149783744+Shinta2374@users.noreply.github.com>
2026-02-22 12:13:29 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,11 @@
 const db = {
-    users: [],
-    messages: [] 
+  users: [
+    { id: 1, username: "shinta", password: "036" },
+    { id: 2, username: "catherine", password: "042" },
+    { id: 3, username: "ibnu", password: "040" },
+    { id: 4, username: "safira", password: "007" },
+  ],
+  messages: [],
 };
 
 module.exports = db;
